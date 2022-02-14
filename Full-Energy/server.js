6673ca70ec22fc65353a038e45baead76bbdd42f
CREpitech <cristian.romero@epitec.eu>
2022-02-14 02:13:27 +0100

    6th commit

--- Full-Energy/server.js
+++ Full-Energy/server.js
@@ -29,17 +29,17 @@ app.use('/api',reviewRoutes);
 app.use('/api',billsRoutes);
 
 
-if(process.env.NODE_ENV === 'production') {
+/* if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', (req, res) => {
       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
-}
+} */
 
 const port = process.env.PORT || 4000;
 const dbURI = config.get('dbURI');
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(port, () => console.log(`Server running on http://localhost:${port}`)))
+  .then(() => app.listen(port, () => console.log(`${port}`)))
   .catch((err) => console.log(err));
 
  
